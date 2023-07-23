4d9b76a365fe011aa3474a89c711030cbe3ffe8c
sergio1880 <11833698-sergio1880@users.noreply.replit.com>
2023-07-23 22:57:57 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int ingresoEntero();
+float ingresoReal();
+void mostrarEntero(int);
+void mostrarReal(float);
+
+int main(void) {
+
+  
+  
+  return 0;
+}
+
+
+int ingresoEntero(){
+  int a;
+  printf("Ingrese un numero Entero:\n");
+  scanf("%d",&a);
+  return a;
+}
+
+float ingresoReal(){
+  float a;
+  printf("Ingrese un numero Real:\n");
+  scanf("%f",&a);
+  return a;
+}
+
+void mostrarEntero(int a){
+  printf("\nEl numero es:%d",a);
+}
+void mostrarReal(float a){
+  printf("\nEl numero es:%f",a);
+}
+
+
+
